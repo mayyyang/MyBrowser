a4b8cda85517d28e3b914ea59dbbccb4534e7a64
May <mayy.yang@gmail.com>
2015-01-20 23:15:10 -0800

    Initial Commit

---
+++ MyBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBrowser
+//
+//  Created by May Yang on 1/20/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
