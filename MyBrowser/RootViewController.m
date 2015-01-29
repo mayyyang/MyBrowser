e0dfed8b4b6888732d35c94056207b7174b2742b
May <mayy.yang@gmail.com>
2015-01-29 01:24:35 -0800

    putting up on Github

--- MyBrowser/RootViewController.m
+++ MyBrowser/RootViewController.m
@@ -12,6 +12,8 @@
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UIButton *goBack;
+@property (weak, nonatomic) IBOutlet UIButton *goForward;
 
 @end
 
@@ -23,6 +25,10 @@
     self.urlTextField.delegate = self;
     [self.activityIndicator hidesWhenStopped];
     [self.activityIndicator stopAnimating];
+    
+    self.urlTextField.text = @"http://";
+    
+    self.urlTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
 }
 
 //- (void)loadRequestFromString:(NSString *)urlString;
@@ -35,12 +41,36 @@
 - (void)webViewDidStartLoad:(UIWebView *)webView
 {
     [self.activityIndicator startAnimating];
+
+    if ([self.webView canGoBack])
+    {
+        [self.goBack setEnabled:YES];
+    }
+    else
+    {
+        [self.goBack setEnabled:NO];
+    }
+    
+    if ([self.webView canGoForward])
+    {
+        [self.goForward setEnabled:YES];
+    }
+    else
+    {
+        [self.goForward setEnabled:NO];
+    }
+    
+    NSString *urlString = [[self.webView.request URL] absoluteString];
+    self.urlTextField.text = urlString;
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
     [self.activityIndicator hidesWhenStopped];
     [self.activityIndicator stopAnimating];
+  
+  
+
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
@@ -57,6 +87,48 @@
     [self.webView loadRequest:urlRequest];
     return YES;
 }
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
 
+    return YES;
+}
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSRange substringRange = [self.urlTextField.text rangeOfString:@"http://"];
+    if (range.location >= substringRange.location && range.location < substringRange.location + substringRange.length)
+    {
+        return NO;
+    }
+    
+    return YES;
+}
+
+#pragma mark - UIBUTTONS
+- (IBAction)backButton:(UIButton *)sender
+{
+    [self.webView goBack];
+}
+
+- (IBAction)forwardButton:(UIButton *)sender
+{
+    [self.webView goForward];
+}
+
+- (IBAction)stopButton:(UIButton *)sender
+{
+    [self.webView stopLoading];
+    [self.activityIndicator stopAnimating];
+    [self.activityIndicator hidesWhenStopped];
+}
+
+- (IBAction)refreshButton:(UIButton *)sender
+{
+    [self.webView reload];
+}
+
+- (IBAction)plusButton:(UIButton *)sender
+{
+    [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Coming Soon!", @"Please fill in all fields") message:NSLocalizedString(@"", @"") delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"OK") otherButtonTitles:nil] show];
+}
 
 @end
